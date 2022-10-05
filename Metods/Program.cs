9231c069caffea95de65c6fc196b30237765faf3
naslanzade <novrastafa@code.edu.az>
2022-10-05 22:36:18 +0400

    add metods

---
+++ Metods/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace Metods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //public static bool DivisibleOfNumbersTo3And7(int num)
+        //{
+        //    if (num % 3 == 0 && num % 7 ==0)
+        //    {
+        //        return (true);
+        //    }
+
+        //    return (false);
+
+        //}
+
+
+
+
+        //public static int SumOfEvenNumbers(int n, int m)
+        //{
+        //    if (n % 2 == 0 && m % 2 == 0)
+        //    {
+        //        return (n + m);
+
+        //    }
+        //    return (0);
+
+        //}
+
+
+
+
+        ////public static int CountOfOddNumbers(int n, int m)
+        ////{
+        ////    if(n > m)
+        ////    {
+        ////        return (0);
+        ////    }
+        ////    else
+        ////    {
+        ////        int count = 0;
+        ////        for (int i = n; i <= m; i++)
+        ////        {
+        ////            if(n % 2 == 1 && m % 2 == 1)
+        ////            {
+        ////                count++;   
+        ////            }
+        ////            return count;
+        ////        }
+
+        ////        return (0);
+        ////    }
+
+        ////}
+
+
+
+        //public static int SumOfOddNumbers(int n, int m)
+        //{
+        //    if (n > m)
+        //    {
+        //        return (0);
+        //    }
+        //    else
+        //    {
+        //        int sum = 0;
+        //        for (int i = n; i <= m; i++)
+        //        {
+        //            if (n % 2 == 1 && m % 2 == 1)
+        //            {
+        //                sum+=i;
+        //            }
+        //            return sum;
+        //        }
+
+        //        return (0);
+        //    }
+
+        //}
+
+
+        //public static int ArraySumOfOddNumbers(int[] nums)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i <= nums.Length; i++)
+        //    {
+        //        if (nums[i] % 2 != 0) ;
+        //        {
+        //            sum += i;
+        //        }
+        //    }
+        //    return sum;
+
+        //}
+
+
+        //    public static int ArrayCountOfEvenNumbers(int[] nums)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i] % 2 == 0) ;
+        //        {
+        //            count++;
+        //        }
+
+        //    }
+        //        return count;
+        //    }
+
+
+        //public static int PrimeOrComplexNumbers(int a)
+        //{
+        //    if (a == 0 || a == 1)
+        //    {
+        //        return 0;
+        //    }
+        //    else
+        //    {
+        //        int count = 0;
+        //        for (int i = 1; i <= a; i++) 
+        //        {
+        //            if (a % 1 == 0)
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (count > 2)
+        //        {
+        //            return a;
+        //        }
+        //        else
+        //        {
+        //            return 0;
+        //        }
+        //    }
+        //}
+
+
+
+
+
+        //  public static int DegreeOfNumber(int n, int i)
+        //{
+        //    if (n<1)
+        //    {
+        //        return 0;
+        //    }
+        //    else
+        //    {
+        //        if (i<n)
+        //        {
+        //            i *= 2;
+        //        }
+        //        return i;
+        //        }
+
+        //    if (i == 0)
+        //    {
+        //        return i;
+        //    }
+        //    else
+        //    {
+        //        return 0;
+        //    }
+        //}
+
+
+
+        //public static int ArrayMultiplicationOfNumbers(int[] nums)
+        //{
+        //    int mult = 1;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i] >= 1 && i <= 20)
+        //        {
+        //            mult *= i;
+        //        }
+        //    }
+        //    return mult;
+        //}
+
+
+
+        //public static int ArrayPowerOfEvenNumbers(int[] nums)
+        //{
+        //    int sum = 0;
+        //    int result = 0;
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (nums[i] % 2 == 0)
+        //        {
+        //            sum += nums[i];
+        //        }
+        //        else
+        //        {
+        //            result = sum + sum;
+        //        }
+        //        return result;
+        //    }
+        //    return 0;
+        //}
+
+
+
+    }
+}
+
